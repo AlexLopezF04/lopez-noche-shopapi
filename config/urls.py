c39e6a14c7290b98d40892151dd7347b6148f10a
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-05-13 19:27:05 -0500

    feat: habilitar panel de admin y configurar rutas base en urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,13 @@
-# store/urls.py
+from django.contrib import admin # <--- 1. Importa el módulo admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from store.views.health import health_check
 
 router = DefaultRouter()
+# Aquí irán tus viewsets más adelante, ej: router.register(r'products', ProductViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls), # <--- 2. Agrega la ruta del administrador
     path('health/', health_check),
     path('', include(router.urls)),
 ]
